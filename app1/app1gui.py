87a937593409b7166d505aa87d046540275922ad
priyankraj <priyank1raj@gmail.com>
2024-10-25 16:24:46 +0530

    Implemented Add button in todo gui

--- app1/app1gui.py
+++ app1/app1gui.py
@@ -1,11 +1,25 @@
-import FreeSimpleGUI as SG
+import FreeSimpleGUI as sg
 import function
 
-label = SG.Text("Type in a to-do")
-input_box = SG.InputText(tooltip="Enter todo")
-add_button = SG.Button("Add")
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
+add_button = sg.Button("Add")
 
-window=SG.Window('My To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
-print('Hello')
+window=sg.Window('My To-Do App',
+                 layout=[[label], [input_box, add_button]],
+                 font=('Helvetica',20))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = function.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            function.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
--- app1/function.py
+++ app1/function.py
@@ -1,8 +1,8 @@
-FILEPATH = "todo_item.txt"
+FILEPATH = "todo.txt"
 
 def get_todos(filepath=FILEPATH):
     with open(filepath,'r') as file_local:
-        todos_local = file_local.readline()
+        todos_local = file_local.readlines()
     return todos_local
 
 def write_todos(todos_arg, filepath=FILEPATH):
